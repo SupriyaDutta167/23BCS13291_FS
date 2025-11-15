051fde12db44cb8def3379db0145bb7478f6492b
SupriyaDutta167 <dsupriya167@gmail.com>
2025-11-15 14:29:25 +0530

    Uploaded the Questions of EST 2

---
+++ PRACTICAL EST/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      {/*<Welcome /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ PRACTICAL EST/my-app/src/Welcome.jsx
@@ -0,0 +1,3 @@
+export default function Welcome({ name = "Guest" }) {
+  return <h1>Welcome {name}!</h1>;
+}
